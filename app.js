74854af1410eebd1b721739e25b14e343e672572
xgeun <112537363+xgeun@users.noreply.github.com>
2023-05-25 16:36:56 +0900

    zz

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ sections.forEach((step, i) => {
     ScrollTrigger.create({
         trigger:step,
         start:"bottom bottom",
-        end: "+=13000",
+        end: "+=2000",
         pin: true,
         anticipatePin: 1,
     });
@@ -16,7 +16,7 @@ let tl = gsap.timeline({
   scrollTrigger: {
     trigger: "video",
     start: "top top",
-    end: "bottom+=2000% bottom",
+    end: "+=7500",
     scrub: true,
   }
 });
